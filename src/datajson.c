23294c27d5e8d385090df862dcf9957f60e2806d
Jesus Antonio Diaz <jesusantonio30122016@gmail.com>
2019-06-28 23:59:26 -0500

    Update datajson.c

--- src/datajson.c
+++ src/datajson.c
@@ -20,33 +20,41 @@ SOFTWARE.
 
 #include <stdio.h>
 #include <stdlib.h>
+#ifndef __TURBOC__
 #include <stdint.h>
-#include "lib/istdas.h"
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
+#endif
+#include <string.h>
 
 FILE *fIn;
 FILE *fOut;
 
-byte dataBuffer;
-byte * secondBuffer;
-byte textSignal;
+unsigned char dataBuffer;
+unsigned char* secondBuffer;
+unsigned char textSignal;
+
+#ifndef __TURBOC__
 uint32_t sizeOfFile;
-uint32_t offset,sOffset,numNot;
+uint32_t offset, sOffset, numNot;
+#endif
+#ifdef __TURBOC__
+long long int sizeOfFile,offset,sOffset,numNot;
+#endif
 unsigned int i;
-const char* coCo[32] =
+char * coCo[32] =
 {
     "NUL","SOH","STX","ETX","ENT","ENQ","ACK","BEL","BS","TAB","LF","VT","FF","CR","SO","SI","DLE",
-    "DC1","DC2","DC3","DC4","NAK","SYN","ETB","CAN","EM","SUB","ESC","FS","GS","RS","US",
+    "DC1","DC2","DC3","DC4","NAK","SYN","ETB","CAN","EM","SUB","ESC","FS","GS","RS","US"
+};
+char * format[8] =
+{
+    "-json","-html"
 };
 
 int main(int argc, char * argv[])
 {
-    if(argc != 2)
+    if(argc != 3)
     {
-        printf("Usage: inputfile\n");
+        printf("Usage: [inputfile] [format]\n");
         return -2;
     }
     offset = 1;
@@ -55,10 +63,7 @@ int main(int argc, char * argv[])
     {
         return -3;
     }
-    fOut = fopen("out.json","wb");
-    fprintf(fOut,"{\n");
-    printf("Writing JSON head\n");
-    textSignal = 0;
+    printf("Writing file head\n");
     secondBuffer = malloc(4096);
     if(secondBuffer == NULL)
     {
@@ -73,44 +78,96 @@ int main(int argc, char * argv[])
     {
         return -4;
     }
-    // For every read char make a registry
-    while(offset < sizeOfFile+3)
+    textSignal = 0;
+    // JSON
+    if(strcmp(format[0],argv[2]) == 0)
     {
-        // wrtie
-        if(offset < 2){ /*Do nothing*/ }
-        else
+        fOut = fopen("out.json","wb");
+        fprintf(fOut,"{\n");
+        // For every read char make a registry
+        while(offset < sizeOfFile+3)
         {
-            fprintf(fOut,"\t\x22%u\x22\n",offset-2);
-            fprintf(fOut,"\t{\n");
-            fprintf(fOut,"\t\t\x22");
-            fprintf(fOut,"hex_value\x22:0x%X\n",dataBuffer);
-            fprintf(fOut,"\t\t\x22");
-            fprintf(fOut,"numeric_value\x22:%d\n",dataBuffer);
-            fprintf(fOut,"\t\t\x22");
-            fprintf(fOut,"char_value\x22:\x22");
-            if(dataBuffer > 32)
-            {
-                fprintf(fOut,"%c",dataBuffer);
-            }
+            // write
+            if(offset < 2){ /*Do nothing*/ }
             else
             {
-                for(i = 0; i < 32; i++)
+                fprintf(fOut,"\t\x22%u\x22\n",offset-2);
+                fprintf(fOut,"\t{\n");
+                fprintf(fOut,"\t\t\x22");
+                fprintf(fOut,"hex_value\x22:0x%X\n",dataBuffer);
+                fprintf(fOut,"\t\t\x22");
+                fprintf(fOut,"numeric_value\x22:%d\n",dataBuffer);
+                fprintf(fOut,"\t\t\x22");
+                fprintf(fOut,"char_value\x22:\x22");
+                if(dataBuffer > 32)
                 {
-                    if(dataBuffer == i)
+                    fprintf(fOut,"%c",dataBuffer);
+                }
+                else
+                {
+                    for(i = 0; i < 32; i++)
                     {
-                        fprintf(fOut,"%s",coCo[i]);
+                        if(dataBuffer == i)
+                        {
+                            fprintf(fOut,"%s",coCo[i]);
+                        }
                     }
                 }
+                fprintf(fOut,"\x22\n");
+                fprintf(fOut,"\t}\n");
+                // read
             }
-            fprintf(fOut,"\x22\n");
-            fprintf(fOut,"\t}\n");
-            // read
+            printf("Writing %d registry entry of %d. (0x%x)\r",offset-2,sizeOfFile,dataBuffer);
+            fread(&dataBuffer,1,1,fIn);
+            offset++;
         }
-        printf("Writing %d registry entry of %d. (0x%x)\r",offset-2,sizeOfFile,dataBuffer);
-        fread(&dataBuffer,1,1,fIn);
-        offset++;
+        fprintf(fOut,"}");
     }
-    fprintf(fOut,"}");
+    // HTML
+    if(strcmp(format[1],argv[2]) == 0)
+    {
+        fOut = fopen("out.html","wb");
+        // HTML Head
+        fprintf(fOut,"<html>\n\t<body>\n\t\t");
+        fprintf(fOut,"\t<style>table, th, td {\n\t\tborder: 1px solid black;\n\t}\t</style>");
+        // Table head
+        fprintf(fOut,"<table>\n\t\t\t<tr>\n\t\t\t\t<td>Offset</td>\n\t\t\t\t<td>Hex Value</td>\n\t\t\t\t<td>Numeric Value</td>\n\t\t\t\t<td>Character</td>\n");
+        // For every read char make a registry
+        while(offset < sizeOfFile+3)
+        {
+            // write
+            if(offset < 2){ /*Do nothing*/ }
+            else
+            {
+                fprintf(fOut,"\t\t\t<tr>\n");
+                fprintf(fOut,"\t\t\t\t<td>%u</td>\n",offset-2);
+                fprintf(fOut,"\t\t\t\t<td>0x%X</td>\n",dataBuffer);
+                fprintf(fOut,"\t\t\t\t<td>%d</td>\n",dataBuffer);
+                fprintf(fOut,"\t\t\t\t<td>");
+                if(dataBuffer > 32)
+                {
+                    fprintf(fOut,"%c",dataBuffer);
+                }
+                else
+                {
+                    for(i = 0; i < 32; i++)
+                    {
+                        if(dataBuffer == i)
+                        {
+                            fprintf(fOut,"%s",coCo[i]);
+                        }
+                    }
+                }
+                fprintf(fOut,"<td>\n");
+                fprintf(fOut,"\t\t\t</tr>\n");
+                // read
+            }
+            printf("Writing %d registry entry of %d. (0x%x)\r",offset-2,sizeOfFile,dataBuffer);
+            fread(&dataBuffer,1,1,fIn);
+            offset++;
+        }
+        fprintf(fOut,"}");
+    }
     fclose(fIn);
     fclose(fOut);
     free(secondBuffer);
