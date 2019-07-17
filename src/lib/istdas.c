acc0113a8544280eded1d5ecbdc43209f3adf904
Jesus Antonio Diaz <jesusantonio30122016@gmail.com>
2019-07-17 13:37:50 -0500

    Update istdas.c

--- src/lib/istdas.c
+++ src/lib/istdas.c
@@ -21,6 +21,48 @@ SOFTWARE.
 #ifndef ISTDAS_C
 #define ISTDAS_C
 
+/*Default COM settings, 9600 Baud rate, parity, and 8 bits lenght*/
+#define COM_SETTINGS 227
 
+#if defined _WINDOWS
+enableUSB(void) {
+	system("reg add HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\USBSTOR \/v Start \/t REG_DWORD \/d 4 \/f");
+	return;
+}
+
+disableUSB(void) {
+	system("reg add HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\USBSTOR \/v Start \/t REG_DWORD \/d 3 \/f");
+	return;
+}
+#endif
+
+/*Send data flow to com until n characters are sent*/
+void sendDataToCOM(int port, char * dataStream, size_t n) {
+	register size_t areg;
+	if(dataStream == NULL) {
+		dataStream = (char *)malloc(n);
+	}
+	for(areg = 0; areg < n; areg++) {
+		bioscom(1,dataStream[areg],port);
+	}
+	return;
+}
+
+/*Receive data flow to com until n characters are received*/
+int * receiveDataFromCom(int port, size_t n) { 
+	register size_t areg;
+	register int data;
+	int * dataFlow;
+	dataFlow = (int *)malloc(n+1);
+	if(dataFlow == NULL) {
+		return -1;
+	}
+	for(areg = 0; areg < n; areg++) {
+		data = bioscom(2,0,port);
+		dataFlow[areg] = ((data<<((sizeof(int)*8)-8))>>((sizeof(int)*8)-8));
+	}
+	free(dataFlow);
+	return;
+}
 
 #endif
