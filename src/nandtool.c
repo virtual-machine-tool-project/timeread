b59379a7df73f851ebaee415881de31cb5014d85
SuperLeaf1995 <jesusantonio30122016@gmail.com>
2020-12-16 23:01:08 -0500

    new functions

--- src/nandtool.c
+++ src/nandtool.c
@@ -10,90 +10,41 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <memory.h>
 #include <fcntl.h>
 #include <sys/time.h>
 #include <time.h>
 #include <unistd.h>
 
-int main(int argc, char ** argv) {
-	int fd;
-	FILE * fp;
-	
-	size_t i;
-	
-	ssize_t r;
-	
-	/* Tempbuffer for NAND reads */
-	unsigned char * tempbuf;
-	
+/* Temporal byte storage*/
+unsigned char temp;
+
+long double * get_cell_degeneration(int fd, const size_t n_cells, const size_t average_time) {
+	/* Timespecs for chrono */
+	struct timespec start, end;
+	/* Cells degeneration array */
 	long double * cells_deg;
-	size_t j;
+	/* Iterators */
+	size_t j, i;
+	int r;
 	
-	/* Timeval struct dynarrays */
-	struct timespec start;
-	struct timespec end;
-	
-	/* Size of buffer */
-	size_t bufsize = 8192;
-	
-	/* Number of cells to take in account for average
-	calculation */
-	size_t n_average_calc = 4096;
-	
-	/* Number of cells to probe of the NAND for degeneration */
-	size_t n_probe = 4096;
-	
-	/* The median time (in ms) for a single access of a cell */
-	size_t average_time;
-	
-	if(argc < 1) {
-		printf("Give [devpath] and optionaly with [outfile] so this works\n");
-		return 1;
-	}
-	
-	tempbuf = malloc(bufsize);
-	if(tempbuf == NULL) {
-		return 1;
-	}
-	
-	printf("Opening %s\n",argv[1]);
-	
-	fd = open(argv[1],O_RDONLY);
-	
-	/* Get timing records */
-	average_time = 0;
-	for(i = 0; i < n_average_calc; i++) {
-		clock_gettime(CLOCK_MONOTONIC,&start);
-		r = read(fd,tempbuf,1);
-		if(r == 0) {
-			return 1;
-		}
-		clock_gettime(CLOCK_MONOTONIC,&end);
-		average_time += (end.tv_nsec-start.tv_nsec);
-	}
-	
-	/* Obtain the average timing for read access */
-	average_time /= n_average_calc;
-	
-	printf("Average reading took %lu nanoseconds\n",average_time);
-	
-	cells_deg = malloc(sizeof(long double)*n_probe);
+	cells_deg = malloc(sizeof(long double)*n_cells);
 	if(cells_deg == NULL) {
-		return 1;
+		return NULL;
 	}
 	
-	for(i = 0; i < n_probe; i++) {
-		cells_deg[i] = 0.0f;
-	}
+	/* Set all to zero */
+	memset(cells_deg,0,n_cells);
 	
 	/* Find degenerated cells */
 	for(j = 0; j < 128; j++) {
 		lseek(fd,SEEK_SET,0);
-		for(i = 0; i < n_probe; i++) {
+		for(i = 0; i < n_cells; i++) {
+			/* Time that the cell took to read */
 			size_t took;
 			
 			clock_gettime(CLOCK_MONOTONIC,&start);
-			r = read(fd,tempbuf,1);
+			r = read(fd,&temp,1);
 			if(r == 0) {
 				break;
 			}
@@ -107,21 +58,145 @@ int main(int argc, char ** argv) {
 		}
 	}
 	
-	/* Write data to file */
-	if(argc == 2) {
-		fp = fopen(argv[2],"wt");
-		for(i = 0; i < n_probe; i++) {
-			fprintf(fp,"%lu::%4.2Lf\n",i,cells_deg[i]);
-		}
-		fclose(fp);
+	return cells_deg;
+}
+
+int get_average_time(int fd, const size_t n_cell) {
+	/* Timespecs for chrono */
+	struct timespec start, end;
+	/* Result holder */
+	ssize_t r;
+	/* Variable representing average time to read a cell in seconds */
+	size_t average_time;
+	int i;
+	
+	/* Get timing records */
+	average_time = 0;
+	for(i = 0; i < n_cell; i++) {
+		/* Start chrono */
+		clock_gettime(CLOCK_MONOTONIC,&start);
 		
-		printf("Data written to %s\n",argv[2]);
+		/* Read a single cell */
+		r = read(fd,&temp,1);
+		if(r == 0) { break; }
+		
+		/* End chrono, calulate time that took to read that cell */
+		clock_gettime(CLOCK_MONOTONIC,&end);
+		average_time += (end.tv_nsec-start.tv_nsec);
 	}
 	
-	free(cells_deg);
+	/* Divide by number of samples evaluated and voila!, we
+	have average reading time */
+	average_time /= i;
 	
-	/* Destroy everything and then end */
-	close(fd);
-	free(tempbuf);
+	/* Return our average time */
+	return average_time;
+}
+
+int main(int argc, char ** argv) {
+	int fd;
+	FILE * fp;
+	size_t i;
+	int r;
+	
+	const char * devpath;
+	const char * outpath;
+	
+	/* Number of cells to probe of the NAND for degeneration */
+	size_t n_probe = 4096;
+	
+	/*
+	-d [file] :: File to put degeneration on
+	-e [numb] :: Number of cells to analyze
+	-a [file] :: File where nand cells are exported (bytes)
+	*/
+	
+	for(i = 1; i < argc; i++) {
+		if(!strcmp(argv[i],"-d")) {
+			long double * cells_deg;
+			
+			outpath = argv[i];
+			
+			i++;
+			if(i >= argc) {
+				printf("Specify file to put degeneration on\n");
+				return 1;
+			}
+			
+			fd = open(devpath,O_RDONLY);
+			if(fd == -1) {
+				perror("cannot use fd");
+				return 1;
+			}
+			
+			cells_deg = get_cell_degeneration(fd,n_probe,get_average_time(fd,n_probe));
+			
+			/* Write data to file */
+			fp = fopen(outpath,"wt");
+			for(i = 0; i < n_probe; i++) {
+				fprintf(fp,"%lu::%4.2Lf\n",i,cells_deg[i]);
+			}
+			fclose(fp);
+			printf("Data written to %s\n",outpath);
+			
+			free(cells_deg);
+			
+			/* Destroy everything and then end */
+			close(fd);
+		} else if(!strcmp(argv[i],"-e")) {
+			i++;
+			if(i >= argc) {
+				printf("Specify number of cells to evaluate\n");
+			}
+			n_probe = atoi(argv[i]);
+		} else if(!strcmp(argv[i],"-a")) {
+			unsigned char * cells;
+			
+			outpath = argv[i];
+			
+			i++;
+			if(i >= argc) {
+				printf("Specify file to put array on\n");
+				return 1;
+			}
+			
+			fd = open(devpath,O_RDONLY);
+			if(fd == -1) {
+				perror("cannot use fd");
+				return 1;
+			}
+			
+			cells = malloc(n_probe+1);
+			if(cells == NULL) {
+				return 1;
+			}
+			
+			/* Read a single cell */
+			r = read(fd,&cells,1);
+			if(r == 0) { break; }
+			
+			/* Write data to file */
+			fp = fopen(outpath,"wb");
+			for(i = 0; i < n_probe; i++) {
+				fwrite(cells,sizeof(unsigned char),n_probe,fp);
+			}
+			fclose(fp);
+			printf("Data written to %s\n",outpath);
+			
+			free(cells);
+			
+			/* Destroy everything and then end */
+			close(fd);
+		} else if(!strcmp(argv[i],"-dev")) {
+			i++;
+			if(i >= argc) {
+				printf("Specify file to use device as\n");
+				return 1;
+			}
+			
+			devpath = argv[i];
+			printf("Using %s as device\n",devpath);
+		}
+	}
 	return 0;
 }
